fee28e72e00fb5d49ee83cc939bde9c6425badf7
victor-430 <oluwatobivictor430@gmail.com>
2025-12-15 21:32:14 +0300

    fix(Navbar): remove class causing the element to move

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-portal-green w-full shadow-lg sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
+      <div className="max-w-7xl  px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-portal-darkGrey text-lg sm:text-xl lg:text-2xl font-medium">
               School Fees Payment Portal
             </h1>
